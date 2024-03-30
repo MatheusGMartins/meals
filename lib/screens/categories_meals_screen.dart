62386624c015fbd62aa457f89ec34111d5464ef0
Matheus Garcia Martins <matheus_gm2@hotmail.com>
2024-03-30 19:07:17 -0300

    Criada modelo de refeição por categoria.

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/adaptative_app_bar.dart';
+import 'package:meals/data/dummy_data.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -8,11 +9,18 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
-    
+
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AdaptativeAppBar(category.title),
-      body: Center(
-        child: Text('Receitas por categoria. ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
